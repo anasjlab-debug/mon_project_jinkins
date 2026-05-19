af883efaa1333708abedcd0beb4765216d4a3106
anasjlab-debug <kevindybryun@gmail.com>
2026-05-19 14:49:07 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("=" * 40)
+print("bienvenue")
+print("=" * 40)
+nom = input("quel est bon ton nom?")
+print(f"bonjour{nom}, ton job jenkins a reussi ")
+
+a = 10
+b = 5
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+
